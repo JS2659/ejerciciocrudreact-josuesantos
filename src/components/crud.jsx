96d9ee8f91001aaa828e84be395b50a5aeee3a94
JS2659 <jisai2558@gmail.com>
2024-09-11 10:34:12 -0600

    Primera parte

--- src/components/crud.jsx
+++ src/components/crud.jsx
@@ -1,28 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function CRUD(){
-     const [id, setId] = useState(0);
-     const [email, setEmail] = useState(0);
-     const [password, setPassword] = useState(0);
-     const [name, setName] = useState(0);
-     const [role, setRole] = useState(0);
-     const [avatar, setAvatar] = useState(0);
+function CRUD() {
+  const [id, setId] = useState(0);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [role, setRole] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [usuarios, setUsuarios] = useState([]);
 
-    return(
-        <div className="container">
-            <div className="row">
-                <h1>Usuarios</h1>
-            </div>
-            <div className="row">
-                <div className="col-3">
-                    <div className="form-floating">
-                        <img src="" alt="" />
-                        <label htmlFor="floatingInputValue">Input with value</label>
-                    </div>
-                </div>
-            </div>
+  const url = "https://api.escuelajs.co/api/v1/users";
+
+  const getUsuarios = async () => {
+    const response = await axios.get(url);
+    setUsuarios(response.data);
+  };
+
+  useEffect(() => {
+    getUsuarios();
+  });
+
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>Usuarios</h1>
+      </div>
+      {usuarios.map((usuario, i) => (
+        <div className="row mt-5 align-items-center">
+          <div className="col-6 col-lg-2 ms-auto">
+            <img
+              src={usuario.avatar}
+              className="img-thumbnail form-control"
+              alt=""
+            />
+            <label htmlFor="">
+              <p>{usuario.name}</p>
+            </label>
+          </div>
+          <div className="col-6 col-lg-4 me-auto">
+            <ul className="list-group">
+              <li className="list-group-item">{usuario.role}</li>
+              <li className="list-group-item">{usuario.email}</li>
+              <li className="list-group-item">{usuario.password}</li>
+            </ul>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default CRUD;
